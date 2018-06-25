d80b72fa08b29c2a3603b8de55dce134d06dc6c2
Taira Terashima <t.terashima@graywings.jp>
2018-06-26 01:02:39 +0900

    First Commit

---
+++ src/bgw.console/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace bgw.console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string defaultFirstCommandString = "-c \"git ";
+            const string defaultLastCommandString = "\"";
+
+            var path = Directory.GetCurrentDirectory();
+
+            var unixPath = new UnixPath(path).path;
+            
+            var bashExePath = Path.Combine(Environment.SystemDirectory, "bash.exe");
+
+            var currentDirectoryCommand = $"cd {unixPath}";
+            var gitCommand = new StringBuilder();
+            gitCommand.Append(defaultFirstCommandString);
+            
+            foreach (var arg in args)
+            {
+                gitCommand.Append(arg);
+                gitCommand.Append(" ");
+            }
+
+            gitCommand.Append(defaultLastCommandString);
+
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = bashExePath,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = false,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    Arguments = currentDirectoryCommand
+                }
+            };
+            process.Start();
+
+            process.StartInfo.Arguments = gitCommand.ToString();
+            process.Start();
+            
+            var results = process.StandardOutput.ReadToEnd();
+            var error = process.StandardError;
+            process.WaitForExit();
+            process.Close();
+            
+            Console.WriteLine(results);
+            
+            while (error.Peek() >= 0)
+            {
+                Console.WriteLine(error.ReadLine());
+            }
+        }
+    }
+}
---
+++ src/bgw.console/UnixPath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace bgw.console
+{
+    public class UnixPath
+    {
+        private const string WindowsPathAnchor = "^(?<driveLetter>.?):(?<directoryPath>.*?)$";
+        private const string WindowsPathSeparator = "\\\\";
+        private const string UnixPathSeparator = "/";
+        private string driveLetter;
+        private readonly string directoryPath;
+        public readonly string path;
+        
+        public UnixPath(string windowsPath)
+        {
+            var pathRegex = new Regex(WindowsPathAnchor);
+            var match = pathRegex.Match(windowsPath);
+            driveLetter = match.Groups["driveLetter"].Value.ToLower();
+            directoryPath = Regex.Replace(match.Groups["directoryPath"].Value, WindowsPathSeparator, UnixPathSeparator);
+            var pathBuilder = new StringBuilder();
+            pathBuilder.Append($"/mnt/{driveLetter}{directoryPath}");
+            path = pathBuilder.ToString();
+        }
+    }
+}
